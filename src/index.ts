c6c0a3539ed0dbc2e2f12e474ed4b41aa46e6f07
nascjoao <oi@joaonasc.dev>
2023-06-19 09:44:46 -0300

    feat: in dev mode allow localhost as origin

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes';
 
+
 dotenv.config();
 const PORT = process.env.PORT || 3001;
+const originAllowed = process.env.NODE_ENV === 'development' ? '*' : 'https://nasc.dev';
 const server = express();
 
 server.use(express.json());
-server.use(cors({ origin: 'https://nasc.dev' }));
+server.use(cors({ origin: originAllowed }));
 server.use('/', router);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
